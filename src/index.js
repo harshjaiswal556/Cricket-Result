3ccbac3cdc7879787bfc869d289286926dc4ea4f
Harsh Shah Jaiswal <tech.harsh2003@gmail.com>
2022-04-08 13:21:34 +0530

    style added

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@
 const express = require('express');
 const port = process.env.PORT || 8000;
 const app = express();
+const path = require('path');
+const templatePath = path.join(__dirname,"/templates")
 
 app.set('view engine', 'hbs');
-app.use(express.urlencoded({ extended: false }));
+app.set("views", templatePath)
+app.use(express.static(templatePath));
 app.use(express.json());
+app.use(express.urlencoded({extended:false}))
 
 app.get('/', (req, res) => {
     res.render('index')
